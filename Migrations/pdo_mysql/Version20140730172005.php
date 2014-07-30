4e9a43c7c8193e1efcab47a5c1e826198ee0b9dc
pyjoe <pierre-yves.legena@student.uclouvain.be>
2014-07-30 17:23:23 +0200

    migration resolution

--- Migrations/pdo_mysql/Version20140730172005.php
+++ Migrations/pdo_mysql/Version20140730172005.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Claroline\ForumBundle\Migrations\pdo_pgsql;
+namespace Claroline\ForumBundle\Migrations\pdo_mysql;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
@@ -8,9 +8,9 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated migration based on mapping information: modify it with caution
  *
- * Generation date: 2014/04/08 11:55:55
+ * Generation date: 2014/07/30 05:20:17
  */
-class Version20140408115553 extends AbstractMigration
+class Version20140730172005 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
@@ -20,13 +20,6 @@ class Version20140408115553 extends AbstractMigration
         ");
         $this->addSql("
             CREATE UNIQUE INDEX UNIQ_2192ACF7E1F029B6 ON claro_forum_category (hash_name)
-        ");
-        $this->addSql("
-            ALTER TABLE claro_forum 
-            ADD hash_name VARCHAR(50) NOT NULL
-        ");
-        $this->addSql("
-            CREATE UNIQUE INDEX UNIQ_F2869DFE1F029B6 ON claro_forum (hash_name)
         ");
         $this->addSql("
             ALTER TABLE claro_forum_message 
@@ -47,28 +40,21 @@ class Version20140408115553 extends AbstractMigration
     public function down(Schema $schema)
     {
         $this->addSql("
-            DROP INDEX UNIQ_F2869DFE1F029B6
-        ");
-        $this->addSql("
-            ALTER TABLE claro_forum 
-            DROP hash_name
-        ");
-        $this->addSql("
-            DROP INDEX UNIQ_2192ACF7E1F029B6
+            DROP INDEX UNIQ_2192ACF7E1F029B6 ON claro_forum_category
         ");
         $this->addSql("
             ALTER TABLE claro_forum_category 
             DROP hash_name
         ");
         $this->addSql("
-            DROP INDEX UNIQ_6A49AC0EE1F029B6
+            DROP INDEX UNIQ_6A49AC0EE1F029B6 ON claro_forum_message
         ");
         $this->addSql("
             ALTER TABLE claro_forum_message 
             DROP hash_name
         ");
         $this->addSql("
-            DROP INDEX UNIQ_273AA20BE1F029B6
+            DROP INDEX UNIQ_273AA20BE1F029B6 ON claro_forum_subject
         ");
         $this->addSql("
             ALTER TABLE claro_forum_subject 
--- Migrations/pdo_pgsql/Version20140730172005.php
+++ Migrations/pdo_pgsql/Version20140730172005.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Claroline\ForumBundle\Migrations\pdo_oci;
+namespace Claroline\ForumBundle\Migrations\pdo_pgsql;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
@@ -8,44 +8,29 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated migration based on mapping information: modify it with caution
  *
- * Generation date: 2014/04/08 11:55:55
+ * Generation date: 2014/07/30 05:20:18
  */
-class Version20140408115553 extends AbstractMigration
+class Version20140730172005 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         $this->addSql("
             ALTER TABLE claro_forum_category 
-            ADD (
-                hash_name VARCHAR2(50) NOT NULL
-            )
+            ADD hash_name VARCHAR(50) NOT NULL
         ");
         $this->addSql("
             CREATE UNIQUE INDEX UNIQ_2192ACF7E1F029B6 ON claro_forum_category (hash_name)
         ");
         $this->addSql("
-            ALTER TABLE claro_forum 
-            ADD (
-                hash_name VARCHAR2(50) NOT NULL
-            )
-        ");
-        $this->addSql("
-            CREATE UNIQUE INDEX UNIQ_F2869DFE1F029B6 ON claro_forum (hash_name)
-        ");
-        $this->addSql("
             ALTER TABLE claro_forum_message 
-            ADD (
-                hash_name VARCHAR2(50) NOT NULL
-            )
+            ADD hash_name VARCHAR(50) NOT NULL
         ");
         $this->addSql("
             CREATE UNIQUE INDEX UNIQ_6A49AC0EE1F029B6 ON claro_forum_message (hash_name)
         ");
         $this->addSql("
             ALTER TABLE claro_forum_subject 
-            ADD (
-                hash_name VARCHAR2(50) NOT NULL
-            )
+            ADD hash_name VARCHAR(50) NOT NULL
         ");
         $this->addSql("
             CREATE UNIQUE INDEX UNIQ_273AA20BE1F029B6 ON claro_forum_subject (hash_name)
@@ -55,32 +40,25 @@ class Version20140408115553 extends AbstractMigration
     public function down(Schema $schema)
     {
         $this->addSql("
-            ALTER TABLE claro_forum 
-            DROP (hash_name)
-        ");
-        $this->addSql("
-            DROP INDEX UNIQ_F2869DFE1F029B6
-        ");
-        $this->addSql("
-            ALTER TABLE claro_forum_category 
-            DROP (hash_name)
-        ");
-        $this->addSql("
             DROP INDEX UNIQ_2192ACF7E1F029B6
         ");
         $this->addSql("
-            ALTER TABLE claro_forum_message 
-            DROP (hash_name)
+            ALTER TABLE claro_forum_category 
+            DROP hash_name
         ");
         $this->addSql("
             DROP INDEX UNIQ_6A49AC0EE1F029B6
         ");
         $this->addSql("
-            ALTER TABLE claro_forum_subject 
-            DROP (hash_name)
+            ALTER TABLE claro_forum_message 
+            DROP hash_name
         ");
         $this->addSql("
             DROP INDEX UNIQ_273AA20BE1F029B6
+        ");
+        $this->addSql("
+            ALTER TABLE claro_forum_subject 
+            DROP hash_name
         ");
     }
 }
